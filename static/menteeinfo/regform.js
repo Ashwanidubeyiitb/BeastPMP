9910e410404b1c484a35179cc94fcd7afa14cf24
Arush <arrush.narayan01@gmail.com>
2024-07-05 23:36:58 +0530

    minor changes done by arush

--- static/menteeinfo/regform.js
+++ static/menteeinfo/regform.js
@@ -14,9 +14,10 @@ $(document).ready(function () {
 		var name = $('#name').val();
 		var roll_no = $('#roll_no').val();
 		var department = $('#department').val();
+		var degree = $('#degree').val();
 		var sop = $('#t04').val();
 		var email = $('#email_id').val();
-		if(name != "" && roll_no!= "" && department != "Department" && sop != "" && email != ""){
+		if(name != "" && roll_no!= "" && department != "---" && degree != "---" && sop != "" && email != ""){
 
 		//Add Class Active
 		$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
@@ -44,8 +45,6 @@ $(document).ready(function () {
 		alert("You need to fill all the details");
 	}
 	});
-
-	
 
 	$(".previous").click(function () {
 
@@ -82,8 +81,78 @@ $(document).ready(function () {
 			.css("width", percent + "%")
 	}
 
-	$(".submit").click(function () {
-		return false;
-	})
+	$("#submit-btn").click(function (event) {
+		// Prevent default form submission (prevents accidental reload)
+		event.preventDefault();
+
+		const preferenceIds = [
+			"#preference_1",
+			"#preference_2",
+			"#preference_3",
+			"#preference_4",
+			"#preference_5"
+		];
+
+		$("#error-message").text(""); // Assuming you have an element with this ID
+
+		// Check if all preferences are filled
+		let allFilled = true;
+		let allNumeric = true;
+		for (const id of preferenceIds) {
+			const value = $(id).val();
+			console.log("value : ", $.isNumeric(value));
+			if(!value){
+				allFilled = false;
+				break;
+			}
+		}
+
+		for (const id of preferenceIds){
+			const val = $(id).val();
+			if (!$.isNumeric(val)) {
+				allNumeric = false;
+				break; // Exit loop if any is empty
+			}
+		}
+		
+		console.log("allFilled : ", allFilled);
+
+		if (allFilled && allNumeric) {
+			// form = $("#msform")[0]
+			// Submit the form (assuming you have a form with ID "msform")
+			$("#msform").submit();
+		} 
+		else if(!allFilled) {
+			// Display error message in the designated div
+			$("#error-message")
+			.text("You have not filled all preference fields.")
+			.css({
+				"background-color": "red",
+				"width": "400px",
+				"margin": "20px auto",
+				"color": "white",
+				"font-family": "Arial, Helvetica, sans-serif",
+				"font-size": "large",
+				"padding": "10px 20px",
+				"border-radius": "2px",
+			});
+
+		}
+		else if(!allNumeric) {
+			// Display error message in the designated div
+			$("#error-message")
+			.text("All preference fields are not numbers")
+			.css({
+				"background-color": "red",
+				"width": "400px",
+				"margin": "20px auto",
+				"color": "white",
+				"font-family": "Arial, Helvetica, sans-serif",
+				"font-size": "large",
+				"padding": "10px 20px",
+				"border-radius": "2px",
+			});
+		}
+	});
 
 });
